ff48a5c7fdba5468d4c336ca0176521733bf7f2b
dharmesh95 <95dharmesh@gmail.com>
2020-07-31 22:37:55 -0400

    testing schema

--- models/Cart.js
+++ models/Cart.js
@@ -1,15 +1,5 @@
 const mongoose = require("mongoose");
 
-//schema
-const CartItemSchema = new mongoose.Schema({
-  id: Number,
-  imageUrl: String,
-  name: String,
-  price: Number,
-  quantity: Number,
-});
-const CartSchema = new mongoose.Schema({
-  name: [CartItemSchema],
-});
+const CartSchema = new mongoose.Schema({}, { strict: false });
 
 module.exports = mongoose.model("Cart", CartSchema);
